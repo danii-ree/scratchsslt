9bf852749a19c582ffeb8c6031f09bcb875b924b
Tom Zhang <tommyawesome08@gmail.com>
2025-04-03 19:21:14 -0400

    firebase config stored securely

---
+++ src/app/firebase/env.js
@@ -0,0 +1,8 @@
+export const info = {
+  apiKey: "AIzaSyBUsCKENfDsxZoz26OL6ygit7DGcCvgI3s",
+  authDomain: "scratcht-database.firebaseapp.com",
+  projectId: "scratcht-database",
+  storageBucket: "scratcht-database.firebasestorage.app",
+  messagingSenderId: "551857954784",
+  appId: "1:551857954784:web:894cd8c9ec3c25dcf5d585",
+};
